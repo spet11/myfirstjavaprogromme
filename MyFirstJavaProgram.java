c773307094a1ab11e005ae3e87216c2a43fdaaec
sanjay patel <nishs1811@gmail.com>
2022-04-02 14:47:52 +0100

    this is my first programme

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,11 @@
+package com.learning.java;
+
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+
+        System.out.print("sanjaykumar");
+        System.out.print(" patel");
+        System.out.println("number");
+
+    }
+}
---
+++ com/ManualTesting.java
@@ -0,0 +1,25 @@
+package com.learning.java.com;
+
+public class ManualTesting {
+    public static void main(String[] args) {
+        System.out.println("What is Software");
+        System.out.println("what is software Testing");
+        System.out.println("who does testing");
+        System.out.println("Qualification of Tester");
+        System.out.println("Challenges Of Tester");
+        System.out.println("SDLC");
+        System.out.println("Software Testing Life Cycle");
+        System.out.println("Principles of Software Testing");
+        System.out.println("Test Plan");
+        System.out.println("Test Scenario");
+        System.out.println("Test Case");
+        System.out.println("Software Testing Technique");
+        System.out.println("What is Bug");
+        System.out.println("what is Severity Vs Priority");
+        System.out.println("Black Box VS White Box");
+        System.out.println("Functional And Non-Functional Testing");
+        System.out.println("Software Development Models");
+
+
+    }
+}
